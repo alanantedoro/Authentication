dec7f2337c1a9c0a78cdc571038b4e88d5296ac1
alanantedoro <alanantedoro@gmail.com>
2021-04-30 00:43:17 -0300

    added google auth, the ability to post secrets and final salt hashing

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
-
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 
 const app = express();
 
@@ -33,22 +34,58 @@ mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema ({
   email: String,
-  password: String
+  password: String,
+  googleId: String,
+  secret: String
 });
 
 ///     Adding the passport local mongoose to our userSchema. We are serilizing and deserializing our user so it put the config into our cookie and then broke it to know which user it is if he comebacks.
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+
+///     Google auth option
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 ///     Routing
 app.get("/", function(req, res){
   res.render("home");
+});
+
+app.get("/auth/google",
+  passport.authenticate("google", {scope: ["profile"]})
+);
+
+app.get("/auth/google/secrets", 
+  passport.authenticate("google", {failureRedirect: "/login"}),
+  function(req, res){
+    res.redirect("/secrets");
 });
 
 app.get("/login", function(req, res){
@@ -60,19 +97,31 @@ app.get("/register", function(req, res){
 });
 
 app.get("/secrets", function(req, res){
-  /// Here we need to see if the user is already loged in or if it needs to.
-  if(req.isAuthenticated()){
-    res.render("secrets");
-  } else {
-    res.redirect("/login");
+User.find({"secret": {$ne: null}}, function(err, foundUsers){
+  if(err){
+    console.log(err);
+  } else{
+    if (foundUsers){
+      res.render("secrets", {usersWithSecrets: foundUsers});
+    }
   }
-})
+});
+});
 
 app.get("/logout", function(req, res){
 /// Deauth the user
 req.logout();
 res.redirect("/");  
 })
+
+
+app.get("/submit", function(req, res){
+  if(req.isAuthenticated()){
+    res.render("submit");
+  } else {
+    res.redirect("/login");
+  }
+});
 
 ///     Encryption by hashing and salting
 
@@ -113,6 +162,22 @@ app.post("/login", function(req, res){
 });
 
 
+app.post("/submit", function(req, res){
+  const submittedSecret = req.body.secret;
+
+  User.findById(req.user.id, function(err, foundUser){
+    if(err){
+      console.log(err);
+    } else{
+      if(foundUser){
+        foundUser.secret = submittedSecret;
+        foundUser.save(function(){
+          res.redirect("/secrets")
+        })
+      }
+    }
+  })
+});
 
 
 
